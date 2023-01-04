273255b098dedc373850ca8854b11b54504d1c05
cheogn97 <cheogn97@gmail.com>
2023-01-04 18:34:38 +0900

    CREATE: 상품 리스트, 상품 추가 기능 dao(GoodsDao, GoodsImgDao)

---
+++ shop_project_semi/src/main/java/dao/GoodsDao.java
@@ -0,0 +1,69 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import vo.Goods;
+
+public class GoodsDao {
+	// 상품 리스트
+	public ArrayList<HashMap<String, Object>> selectGoodsList(Connection conn) throws Exception {
+		ArrayList<HashMap<String, Object>> list = new ArrayList<>();
+		String sql = "SELECT goods_name goodsName, goods_price goodsPrice, filename FROM goods gs JOIN goods_img gsi ON gs.goods_code = gsi.goods_code";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		ResultSet rs = stmt.executeQuery();
+		
+		while(rs.next()) {
+			HashMap<String, Object> m = new HashMap<>();
+			m.put("goodsName", rs.getString("goodsName"));
+			m.put("goodsPrice", rs.getInt("goodsPrice"));
+			m.put("filename", rs.getString("fileName"));
+			list.add(m);
+		}
+		
+		return list;
+	}
+	
+	// 상품 상세 정보
+	public void selectGoodsOne() throws Exception {
+		
+	}
+	
+	// 상품 수정
+	public void updateGoods() throws Exception {
+		
+	}
+	
+	// 상품 삭제
+	public void deleteGoods() throws Exception {
+		
+	}
+	
+	// 상품 추가
+	public HashMap<String, Integer> insertGoods(Connection conn, Goods goods) throws Exception {
+		String sql = "INSERT INTO goods(goods_name, goods_price, sold_out, emp_id, hit, createdate, updatedate) VALUES(?, ?, ?, ?, ?, NOW(), NOW())";
+		PreparedStatement stmt = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+		stmt.setString(1, goods.getGoodsName());
+		stmt.setInt(2, goods.getGoodsPrice());
+		stmt.setString(3, goods.getSoldOut());
+		stmt.setString(4, goods.getEmpId());
+		stmt.setInt(5, goods.getHit());
+		
+		int result = stmt.executeUpdate();
+		ResultSet rs = stmt.getGeneratedKeys(); 
+		
+		int autoKey = 0; 
+		if(rs.next()) {
+			autoKey = rs.getInt(1); // goods.goods_code AUTO_INCREMENT
+		}
+		
+		HashMap<String, Integer> m = new HashMap<>();
+		m.put("result", result);
+		m.put("autoKey", autoKey);
+		
+		return m;
+	}
+}
---
+++ shop_project_semi/src/main/java/dao/GoodsImgDao.java
@@ -0,0 +1,25 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import vo.GoodsImg;
+
+public class GoodsImgDao {
+	// 상품 추가
+	public int insertGoodsImg(Connection conn, GoodsImg goodsImg) throws Exception {
+		String sql = "INSERT INTO goods_img(goods_code, filename, origin_name, content_type, createdate, updatedate) "
+				+ "VALUES(?, ?, ?, ?, (SELECT createdate FROM goods WHERE goods_code = "+ goodsImg.getGoodsCode() +"), "
+						+ "(SELECT updatedate FROM goods WHERE goods_code = "+ goodsImg.getGoodsCode() +") )";
+		
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setInt(1, goodsImg.getGoodsCode());
+		stmt.setString(2, goodsImg.getFileName());
+		stmt.setString(3, goodsImg.getOriginName());
+		stmt.setString(4, goodsImg.getContentType());
+		
+		int result = stmt.executeUpdate();
+		
+		return result;
+	}
+}
