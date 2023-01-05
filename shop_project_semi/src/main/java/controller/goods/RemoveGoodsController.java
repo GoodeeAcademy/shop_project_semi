eaff64460dce364101b2647ae8d03fb58c29168e
cheogn97 <cheogn97@gmail.com>
2023-01-05 14:27:03 +0900

    EDIT: import 삭제

--- shop_project_semi/src/main/java/controller/goods/RemoveGoodsController.java
+++ shop_project_semi/src/main/java/controller/goods/RemoveGoodsController.java
@@ -2,7 +2,6 @@ package controller.goods;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
