5b9d31522fd02871043f3c63b28449fee561f06a
cheogn97 <cheogn97@gmail.com>
2023-01-12 10:20:47 +0900

    [최정헌] order테이블 order_state 필드 -> order_goods테이블로 이동

--- shop_project_semi/src/main/java/vo/Orders.java
+++ shop_project_semi/src/main/java/vo/Orders.java
@@ -6,81 +6,63 @@ public class Orders {
 	private String orderName;
 	private String address;
 	private int orderPrice;
-	private String orderState;
 	private String createdate;
 	
+	public Orders() {
+		super();
+	}
+	
 	public Orders(int orderCode, String customerId, String orderName, String address, int orderPrice,
-			String orderState, String createdate) {
+			String createdate) {
 		super();
 		this.orderCode = orderCode;
 		this.customerId = customerId;
 		this.orderName = orderName;
 		this.address = address;
 		this.orderPrice = orderPrice;
-		this.orderState = orderState;
 		this.createdate = createdate;
 	}
-
-	public Orders() {
-		super();
-		// TODO Auto-generated constructor stub
+	
+	@Override
+	public String toString() {
+		return "Orders [orderCode=" + orderCode + ", customerId=" + customerId + ", orderName=" + orderName
+				+ ", address=" + address + ", orderPrice=" + orderPrice + ", createdate=" + createdate + "]";
 	}
-
+	
 	public int getOrderCode() {
 		return orderCode;
 	}
-
 	public void setOrderCode(int orderCode) {
 		this.orderCode = orderCode;
 	}
-
 	public String getCustomerId() {
 		return customerId;
 	}
-
 	public void setCustomerId(String customerId) {
 		this.customerId = customerId;
 	}
-
 	public String getOrderName() {
 		return orderName;
 	}
-
 	public void setOrderName(String orderName) {
 		this.orderName = orderName;
 	}
-
 	public String getAddress() {
 		return address;
 	}
-
 	public void setAddress(String address) {
 		this.address = address;
 	}
-
 	public int getOrderPrice() {
 		return orderPrice;
 	}
-
 	public void setOrderPrice(int orderPrice) {
 		this.orderPrice = orderPrice;
 	}
-
-	public String getOrderState() {
-		return orderState;
-	}
-
-	public void setOrderState(String orderState) {
-		this.orderState = orderState;
-	}
-
 	public String getCreatedate() {
 		return createdate;
 	}
-
 	public void setCreatedate(String createdate) {
 		this.createdate = createdate;
 	}
-	
-	
 }
