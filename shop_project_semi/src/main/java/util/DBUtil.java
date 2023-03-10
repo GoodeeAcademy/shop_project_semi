940e31a2985a0bddc8dce17ad9acc3bcede7b78a
M1nsong-Kim <lgms.3222@gmail.com>
2023-03-10 13:58:52 +0900

    [김민송] DB 경로 주석 추가

--- shop_project_semi/src/main/java/util/DBUtil.java
+++ shop_project_semi/src/main/java/util/DBUtil.java
@@ -7,6 +7,7 @@ public class DBUtil {
 	public static Connection getConnection() throws Exception {
 		//Class.forName("org.mariadb.jdbc.Driver");
 		Connection conn = DriverManager.getConnection("jdbc:mariadb://3.39.47.155:3306/shop","root","java1234");
+		//Connection conn = DriverManager.getConnection("jdbc:mariadb://13.124.174.73:3306/shop","root","java1234");
 		conn.setAutoCommit(false);
 		
 		return conn;
